2c5075c802361a6445a2d342bdbba3986ee8d9c9
Leo <admin@MacBook-Pro-de-Admin.local>
2018-06-08 21:30:55 -0300

    Version 5 mapa y contacto sombra

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 			scrollTop: $($.attr(this, 'href')).offset().top
 		}, 1000);
 	});
+	$('#numero').hide();
 	$('#numero').removeClass("whatsappon");
 	$("#whatsapp").click(function(){
+		$('#numero').show();
 		$('#numero').toggleClass("whatsappon");
 	});
 });
